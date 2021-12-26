4cab9a484d3416d5c25885f2302c71e6f6660357
Alexander Garuba <alexandergaruba96@gmail.com>
2021-12-26 01:21:58 +0100

    setting up github actions

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,22 +1,25 @@
 import { useState, useContext } from "react";
 import AlertContext from "../../context/alert/AlertContext";
 import GithubContext from "../../context/github/GitHubContext";
+import { searchUsers } from "../../context/github/GithubActions";
 
 function UserSearch() {
   const [text, setText] = useState("");
 
-  const { users, clearUsers, searchUsers } = useContext(GithubContext);
+  const { users, dispatch, clearUsers } = useContext(GithubContext);
   const { setAlert } = useContext(AlertContext);
 
   const handleChange = (e) => setText(e.target.value);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (text === "") {
       setAlert("Please enter something", "error");
     } else {
-      searchUsers(text);
+      dispatch({ type: "SET_LOADING" });
+      const users = await searchUsers(text);
+      dispatch({ type: "GET_USERS", payload: users });
 
       setText("");
     }
