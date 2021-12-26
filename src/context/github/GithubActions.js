c4257a30783bf0360fc047ebf75423152ef262f9
Alexander Garuba <alexandergaruba96@gmail.com>
2021-12-26 01:33:08 +0100

    cleanup github context

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -17,3 +17,35 @@ export const searchUsers = async (text) => {
 
   return items;
 };
+
+// Get single user
+export const getUser = async (login) => {
+  const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+    headers: {
+      Authorization: `token ${GITHUB_TOKEN}`,
+    },
+  });
+
+  if (response.status === 404) {
+    window.location = "/notfound";
+  } else {
+    const data = await response.json();
+    return data;
+  }
+};
+
+// Get single user repos
+export const getUserRepos = async (login) => {
+  const response = await fetch(`${GITHUB_URL}/users/${login}/repos`, {
+    headers: {
+      Authorization: `token ${GITHUB_TOKEN}`,
+    },
+  });
+
+  if (response.status === 404) {
+    window.location = "/notfound";
+  } else {
+    const data = await response.json();
+    return data;
+  }
+};
